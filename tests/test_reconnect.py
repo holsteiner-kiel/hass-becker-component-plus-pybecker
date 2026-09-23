8285b76031344ce5ea21c08ccbe121d3585580a8
holsteiner-kiel <83506028+holsteiner-kiel@users.noreply.github.com>
2026-09-23 08:05:11 +0200

    Initialize availability state in reconnect tests

--- tests/test_reconnect.py
+++ tests/test_reconnect.py
@@ -36,6 +36,8 @@ def _build_communicator(connection: _FlakyConnection) -> BeckerCommunicator:
     communicator._force_stop_flag = threading.Event()
     communicator._write_queue = queue.Queue(maxsize=10)
     communicator._callback = lambda packet: None
+    communicator._availability_callback = None
+    communicator._last_available = None
     communicator._connection = connection
     communicator._read_buffer = bytes()
     communicator._timeout = 0.0
